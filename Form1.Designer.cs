bb624187ee0c4b252f26a955c99b02a9e7667bbb
ahmmedabdelalim <am909962@gmail.com>
2021-05-25 22:51:09 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace circle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Xc = new System.Windows.Forms.Label();
+            this.Yc = new System.Windows.Forms.Label();
+            this.Radius = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.Rx = new System.Windows.Forms.Label();
+            this.Ry = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Xc
+            // 
+            this.Xc.AutoSize = true;
+            this.Xc.Location = new System.Drawing.Point(95, 51);
+            this.Xc.Name = "Xc";
+            this.Xc.Size = new System.Drawing.Size(20, 15);
+            this.Xc.TabIndex = 0;
+            this.Xc.Text = "Xc";
+            this.Xc.Click += new System.EventHandler(this.Xc_Click);
+            // 
+            // Yc
+            // 
+            this.Yc.AutoSize = true;
+            this.Yc.Location = new System.Drawing.Point(340, 51);
+            this.Yc.Name = "Yc";
+            this.Yc.Size = new System.Drawing.Size(19, 15);
+            this.Yc.TabIndex = 1;
+            this.Yc.Text = "Yc";
+            // 
+            // Radius
+            // 
+            this.Radius.AutoSize = true;
+            this.Radius.Location = new System.Drawing.Point(548, 51);
+            this.Radius.Name = "Radius";
+            this.Radius.Size = new System.Drawing.Size(42, 15);
+            this.Radius.TabIndex = 2;
+            this.Radius.Text = "Radius";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(173, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(405, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(625, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Draw Circle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(152, 141);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(551, 297);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(173, 92);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(405, 91);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 9;
+            // 
+            // Rx
+            // 
+            this.Rx.AutoSize = true;
+            this.Rx.Location = new System.Drawing.Point(95, 100);
+            this.Rx.Name = "Rx";
+            this.Rx.Size = new System.Drawing.Size(20, 15);
+            this.Rx.TabIndex = 10;
+            this.Rx.Text = "Rx";
+            // 
+            // Ry
+            // 
+            this.Ry.AutoSize = true;
+            this.Ry.Location = new System.Drawing.Point(340, 95);
+            this.Ry.Name = "Ry";
+            this.Ry.Size = new System.Drawing.Size(20, 15);
+            this.Ry.TabIndex = 11;
+            this.Ry.Text = "Ry";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(687, 91);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Draw ellipse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Ry);
+            this.Controls.Add(this.Rx);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Radius);
+            this.Controls.Add(this.Yc);
+            this.Controls.Add(this.Xc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Xc;
+        private System.Windows.Forms.Label Yc;
+        private System.Windows.Forms.Label Radius;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label Rx;
+        private System.Windows.Forms.Label Ry;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace circle
+{
+    public partial class Form1 : Form
+    {
+
+        public void circle( Point center , double r)
+        {
+            Bitmap bic = new Bitmap(this.Width, this.Height);
+
+
+            for(int thate = 0; thate<=360; thate++)
+            {
+                double x, y;
+
+                x = center.X + r * Math.Cos(thate);
+
+                y = center.Y + r  * Math.Sin(thate);
+
+                bic.SetPixel((int)(x), (int)(y), Color.Red);
+
+            }
+            pictureBox1.Image = bic;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Xc_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Xc = Convert.ToInt32(textBox1.Text);
+            int Yc = Convert.ToInt32(textBox2.Text);
+
+            double r = Convert.ToDouble(textBox3.Text);
+
+            Point center = new Point(Xc, Yc);
+
+            circle(center, r);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+         
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int X = Convert.ToInt32(textBox1.Text);
+            int Y = Convert.ToInt32(textBox2.Text);
+            int Rx = Convert.ToInt32(textBox4.Text);
+            int Ry = Convert.ToInt32(textBox5.Text);
+
+            Point p1 = new Point(X, Y);
+            Bitmap Bit = new Bitmap(this.Width, this.Height);
+
+            ellipseMidpoint(p1, Rx, Ry, Bit);
+
+
+
+
+        }
+
+        void ellipseMidpoint(Point p1, int Rx, int Ry, Bitmap Bit)
+        {
+            int Rx2 = Rx * Rx;
+            int Ry2 = Ry * Ry;
+            int twoRx2 = 2 * Rx2;
+            int twoRy2 = 2 * Ry2;
+            double p;
+            int x = 0;
+            int y = Ry;
+            int px = 0;
+            int py = twoRx2 * y;
+
+            p = Math.Round(Ry2 - (Rx2 * Ry) + (0.25 * Rx2));
+            ellipsePlotPoints(p1.X, p1.Y, x, y, Bit);
+
+            while (px < py)
+            {
+                x++;
+                px += twoRy2;
+                if (p < 0)
+                    p += Ry2 + px;
+                else
+                {
+                    y--;
+                    py -= twoRx2;
+                    p += Ry2 + px - py;
+                }
+
+                ellipsePlotPoints(p1.X, p1.Y, x, y, Bit);
+            }
+
+            /* Region 2 */
+            p = Math.Round(Ry2 * (x + 0.5) * (x + 0.5) + Rx2 * (y - 1) * (y - 1) - Rx2 * Ry2);
+            while (y > 0)
+            {
+                y--;
+                py -= twoRx2;
+                if (p > 0)
+                    p += Rx2 - py;
+                else
+                {
+                    x++;
+                    px += twoRy2;
+                    p += Rx2 - py + px;
+                }
+
+                ellipsePlotPoints(p1.X, p1.Y, x, y, Bit);
+            }
+        }
+        private void ellipsePlotPoints(int xCenter, int yCenter, int x, int y, Bitmap Bit)
+        {
+
+            Bit.SetPixel(xCenter + x, yCenter + y, Color.Red);
+            Bit.SetPixel(xCenter - x, yCenter + y, Color.Red);
+            Bit.SetPixel(xCenter + x, yCenter - y, Color.Red);
+            Bit.SetPixel(xCenter - x, yCenter - y, Color.Red);
+            pictureBox1.Image = Bit;
+        }
+    }
+}
